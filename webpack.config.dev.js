a609840c1b34fbce3f990ce06b314a4834b84948
zearkiatos <caprilespe@outlook.com>
2022-01-10 22:41:27 -0300

    Webpack Bundle Analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const Dotenv = require("dotenv-webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: "./src/index.js",
@@ -40,6 +41,7 @@ module.exports = {
         }
       ]
     }),
+    new BundleAnalyzerPlugin()
   ],
   devServer: {
     static: {
